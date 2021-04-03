58b8f0834b55c5867b4d683c8f52fb318fd8ebcb
sabamosleh <saba_mosleh1996@yahoo.com>
2021-04-03 15:23:58 +0430

    f6

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,2 +1,23 @@
-importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.3.2/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBraehDytmyUtTOdvL4W5qcceE9njqMdVQ",
+    authDomain: "fir-notif-test.firebaseapp.com",
+    projectId: "fir-notif-test",
+    storageBucket: "fir-notif-test.appspot.com",
+    messagingSenderId: "508125997354",
+    appId: "1:508125997354:web:a95e81c801a935a3e9d653",
+    measurementId: "G-EWNXXS9M50"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
+</script>
